55060122ea7270f3ed241c157c169cbd68f5e0fb
andreza-vilar <andreza.farias@ccc.ufcg.edu.br>
2024-08-25 01:43:23 -0300

    add: itens bomboniere

--- ClientInterface.hs
+++ ClientInterface.hs
@@ -1,6 +1,7 @@
 module ClientInterface where
 
 import System.IO (readFile)
+import Data.List (isPrefixOf)
 
 -- Função para visualizar filmes
 viewMovies :: IO ()
@@ -8,6 +9,23 @@ viewMovies = do
   contents <- readFile "movies.txt"
   putStrLn "Lista de filmes disponíveis:"
   putStrLn contents
+
+-- Função para visualizar itens da bomboniere
+viewItems :: IO ()
+viewItems = do
+  contents <- readFile "concessionStand.txt"
+  putStrLn "Itens da bomboniere disponíveis:"
+  putStrLn contents
+
+-- Função para comprar um item da bomboniere
+buyItem :: IO ()
+buyItem = do
+  viewItems
+  putStrLn "Digite o nome do item que deseja comprar:"
+  itemName <- getLine
+  -- Simulação de confirmação de compra
+  putStrLn ("Você comprou o item: " ++ itemName)
+  -- Aqui você pode adicionar lógica para realmente processar a compra, se necessário
 
 -- Outras funções do cliente
 buyTicket :: IO ()
@@ -22,12 +40,14 @@ runClientMode = do
   putStrLn "Modo Cliente:"
   putStrLn "1) Visualizar Filmes"
   putStrLn "2) Comprar Ingresso"
-  putStrLn "3) Deixar Feedback"
-  putStrLn "4) Voltar ao Menu Principal"
+  putStrLn "3) Comprar Item da Bomboniere"
+  putStrLn "4) Deixar Feedback"
+  putStrLn "5) Voltar ao Menu Principal"
   option <- getLine
   case option of
     "1" -> viewMovies >> runClientMode
     "2" -> buyTicket >> runClientMode
-    "3" -> giveFeedback >> runClientMode
-    "4" -> return () -- Retorna ao menu principal no módulo Main
+    "3" -> buyItem >> runClientMode
+    "4" -> giveFeedback >> runClientMode
+    "5" -> return () -- Retorna ao menu principal
     _   -> putStrLn "Opção inválida. Tente novamente." >> runClientMode
--- ConcessionStand.hs
+++ ConcessionStand.hs
@@ -1,17 +1,79 @@
 module ConcessionStand where
 
+import System.IO (readFile, writeFile, appendFile)
+import Data.List (isPrefixOf)
+
+-- Arquivo onde os itens da bomboniere serão armazenados
+fileName :: FilePath
+fileName = "concessionStand.txt"
+
+-- Função para adicionar um item à bomboniere
 addItem :: IO ()
-addItem = putStrLn "Adicionar um novo item na bomboniere."
+addItem = do
+  putStrLn "Digite o nome do item:"
+  itemName <- getLine
+  putStrLn "Digite o preço do item (em reais):"
+  price <- getLine
+  let itemEntry = itemName ++ " | R$ " ++ price ++ "\n"
+  appendFile fileName itemEntry
+  putStrLn "Item adicionado com sucesso."
 
+-- Função para listar todos os itens da bomboniere
+listItems :: IO ()
+listItems = do
+  contents <- readFile fileName
+  putStrLn "Itens da bomboniere:"
+  putStrLn contents
+
+-- Função para editar um item na bomboniere
+editItem :: IO ()
+editItem = do
+  listItems
+  putStrLn "Digite o nome do item a ser editado:"
+  oldName <- getLine
+  putStrLn "Digite o novo nome do item (ou deixe em branco para manter o mesmo):"
+  newName <- getLine
+  putStrLn "Digite o novo preço do item (ou deixe em branco para manter o mesmo):"
+  newPrice <- getLine
+  contents <- readFile fileName
+  let updatedItems = map (updateItem oldName newName newPrice) (lines contents)
+  writeFile fileName (unlines updatedItems)
+  putStrLn "Item atualizado com sucesso."
+
+-- Função auxiliar para atualizar um item
+updateItem :: String -> String -> String -> String -> String
+updateItem oldName newName newPrice line
+  | oldName `isPrefixOf` line =
+      let (name, price) = break (== '|') line
+          updatedName = if null newName then name else newName
+          updatedPrice = if null newPrice then drop 1 price else newPrice
+      in updatedName ++ " | R$ " ++ updatedPrice
+  | otherwise = line
+
+-- Função para remover um item da bomboniere
 removeItem :: IO ()
-removeItem = putStrLn "Remover um item da bomboniere."
+removeItem = do
+  listItems
+  putStrLn "Digite o nome do item a ser removido:"
+  itemToRemove <- getLine
+  contents <- readFile fileName
+  let updatedItems = filter (not . isPrefixOf itemToRemove) (lines contents)
+  writeFile fileName (unlines updatedItems)
+  putStrLn "Item removido com sucesso."
 
+-- Menu de gerenciamento da bomboniere
 manageConcessionStand :: IO ()
 manageConcessionStand = do
   putStrLn "1) Adicionar Item"
-  putStrLn "2) Remover Item"
+  putStrLn "2) Listar Itens"
+  putStrLn "3) Editar Item"
+  putStrLn "4) Remover Item"
+  putStrLn "5) Voltar ao Menu Principal"
   option <- getLine
   case option of
-    "1" -> addItem
-    "2" -> removeItem
-    _   -> putStrLn "Opção inválida. Tente novamente."
+    "1" -> addItem >> manageConcessionStand
+    "2" -> listItems >> manageConcessionStand
+    "3" -> editItem >> manageConcessionStand
+    "4" -> removeItem >> manageConcessionStand
+    "5" -> return () -- Volta ao menu principal
+    _   -> putStrLn "Opção inválida. Tente novamente." >> manageConcessionStand
