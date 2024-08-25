ffd718b44972e26b6c15aa9869b8a4eb6cfb019c
andreza-vilar <andreza.farias@ccc.ufcg.edu.br>
2024-08-25 02:49:34 -0300

    add: funcionalidade add filme

--- MovieManagement.hs
+++ MovieManagement.hs
@@ -1,14 +1,28 @@
 module MovieManagement where
 
-addMovie :: IO ()
-addMovie = putStrLn "Adicionar um novo filme ao sistema."
+import System.IO (appendFile)
 
+-- Função para adicionar um filme
+addMovie :: IO ()
+addMovie = do
+  putStrLn "Digite o título do filme:"
+  title <- getLine
+  putStrLn "Digite o diretor do filme:"
+  director <- getLine
+  putStrLn "Digite o ano de lançamento:"
+  year <- getLine
+  let movieEntry = title ++ " | " ++ director ++ " | " ++ year ++ "\n"
+  appendFile "movies.txt" movieEntry
+  putStrLn "Filme adicionado com sucesso."
+
+-- Funções para editar e remover filme (mantidas como estão)
 editMovie :: IO ()
 editMovie = putStrLn "Editar informações de um filme existente."
 
 removeMovie :: IO ()
 removeMovie = putStrLn "Remover um filme do sistema."
 
+-- Menu de gerenciamento de filmes
 manageMovies :: IO ()
 manageMovies = do
   putStrLn "1) Adicionar Filme"
