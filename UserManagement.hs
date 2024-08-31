d7a00c42b1c92641b7bf22612798d05fbeb45b93
edvaaaan <edvanfilho1400@gmail.com>
2024-08-31 12:32:28 -0300

    usuarios teste v1 - edvan

--- UserManagement.hs
+++ UserManagement.hs
@@ -1,22 +1,37 @@
 module UserManagement where
+import System.IO
 
+-- Adiciona um novo usuário ao arquivo "usersCinema.txt"
 addUser :: IO ()
-addUser = putStrLn "Adicionar um novo usuário."
+addUser = do
+  putStrLn "Digite o email do novo usuário:"
+  userEmail <- getLine
+  putStrLn "Digite a quantidade de pontos do novo usuário:"
+  userPoints <- getLine
+  appendFile "usersCinema.txt" (userEmail ++ " " ++ userPoints ++ "\n")
+  putStrLn "Usuário adicionado com sucesso."
 
-editUser :: IO ()
-editUser = putStrLn "Editar perfil de um usuário."
+-- Edita um dos usuários do arquivo "usersCinema.txt"
+editUser :: IO()
+editUser = do
+  putStrLn "teste"
 
-removeUser :: IO ()
-removeUser = putStrLn "Remover um usuário do sistema."
+-- Remove o usuário do arquivo "usersCinema.txt"
+removeUser :: IO()
+removeUser = do
+  putStrLn "teste"
 
+-- Função principal de gerenciamento de usuários
 manageUsers :: IO ()
 manageUsers = do
   putStrLn "1) Adicionar Usuário"
   putStrLn "2) Editar Usuário"
   putStrLn "3) Remover Usuário"
+  putStrLn "4) Voltar"
   option <- getLine
   case option of
-    "1" -> addUser
-    "2" -> editUser
-    "3" -> removeUser
-    _   -> putStrLn "Opção inválida. Tente novamente."
+    "1" -> addUser >> manageUsers
+    "2" -> editUser >> manageUsers
+    "3" -> removeUser >> manageUsers
+    "4" -> putStrLn "Voltando ao menu anterior."
+    _   -> putStrLn "Opção inválida. Tente novamente." >> manageUsers
