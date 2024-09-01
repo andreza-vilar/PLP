88cfb056a3cdd540baceac442e858253c1abc9fb
edvaaaan <edvanfilho1400@gmail.com>
2024-09-01 10:45:27 -0300

    usuarios completo 2024 att

--- UserManagement.hs
+++ UserManagement.hs
@@ -1,29 +1,66 @@
 module UserManagement where
-import System.IO
 
--- Adiciona um novo usuário ao arquivo "usersCinema.txt"
+import System.IO (appendFile, readFile, writeFile)
+import Data.List (isInfixOf)
+
+-- Função para adicionar um usuário
 addUser :: IO ()
 addUser = do
-  putStrLn "Digite o email do novo usuário:"
-  userEmail <- getLine
-  putStrLn "Digite a quantidade de pontos do novo usuário:"
-  userPoints <- getLine
-  appendFile "usersCinema.txt" (userEmail ++ " " ++ userPoints ++ "\n")
+  putStrLn "Digite o e-mail do usuário:"
+  email <- getLine
+  putStrLn "Digite a pontuação do usuário:"
+  points <- getLine
+  let userEntry = "Usuario: " ++ email ++ " | Pontos: " ++ points ++ "\n"
+  appendFile "usersCinema.txt" userEntry
   putStrLn "Usuário adicionado com sucesso."
 
--- Edita um dos usuários do arquivo "usersCinema.txt"
-editUser :: IO()
+-- Lista os usuários para facilitar a visualização na hora de remover
+listUsers :: IO [String]
+listUsers = do
+  contents <- readFile "usersCinema.txt"
+  let users = filter (isInfixOf "Usuário: ") (lines contents)
+  putStrLn "Lista de usuários:"
+  mapM_ putStrLn users
+  return users
+
+-- Função para editar um usuário existente
+editUser :: IO ()
 editUser = do
-  putStrLn "teste"
+  users <- listUsers
+  putStrLn "Digite o e-mail do usuário a ser editado:"
+  emailToEdit <- getLine
+  let userToEdit = filter (isInfixOf emailToEdit) users
+  if null userToEdit
+    then putStrLn "Usuário não encontrado."
+    else do
+      putStrLn "Digite o novo e-mail do usuário:"
+      newEmail <- getLine
+      putStrLn "Digite a nova pontuação do usuário:"
+      newPoints <- getLine
+      let newUserEntry = "Usuario: " ++ newEmail ++ " | Pontos: " ++ newPoints
+      let updatedUsers = map (\user -> if isInfixOf emailToEdit user then newUserEntry else user) users
+      writeFile "usersCinema.txt" (unlines updatedUsers)
+      putStrLn "Usuário editado com sucesso."
 
--- Remove o usuário do arquivo "usersCinema.txt"
-removeUser :: IO()
+-- Função para remover um usuário
+removeUser :: IO ()
 removeUser = do
-  putStrLn "teste"
+  users <- listUsers
+  putStrLn "Digite o e-mail do usuário a ser removido:"
+  emailToRemove <- getLine
+  let userToRemove = filter (isInfixOf emailToRemove) users
+  if null userToRemove
+    then putStrLn "Usuário não encontrado."
+    else do
+      let updatedUsers = filter (not . isInfixOf emailToRemove) users
+      writeFile "usersCinema.txt" (unlines updatedUsers)
+      putStrLn "Usuário removido com sucesso."
 
--- Função principal de gerenciamento de usuários
+
+-- Função para gerenciar usuários
 manageUsers :: IO ()
 manageUsers = do
+  putStrLn "Gerenciamento de Usuários:"
   putStrLn "1) Adicionar Usuário"
   putStrLn "2) Editar Usuário"
   putStrLn "3) Remover Usuário"
